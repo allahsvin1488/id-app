4d75461144e28c579619d078b3617f202055e37a
allahsvin1488 <potatoman2010@mail.ru>
2026-05-21 11:09:05 +0300

    Update video file for first level and uncomment code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const levels = [
-    { answer: 'plant', video: 'cube.mp4' },
+    { answer: 'plant', video: 'sample_holiday_trip.mp4' },
     { answer: 'cube', video: 'planet.mp4' },
     { answer: 'planet', video: '' } 
 ];
@@ -7,25 +7,25 @@ const levels = [
 
 function check(event) {
     event.preventDefault();
-  // let score = document.getElementById('score');
-   // let level = document.getElementById('level'); 
-  //  const inputValue = event.target.userAnswer.value;
- //   const video = document.getElementById('videoPlayer');
-   // const currentLevel = Number(level.value) - 1;
+  let score = document.getElementById('score');
+   let level = document.getElementById('level'); 
+   const inputValue = event.target.userAnswer.value;
+   const video = document.getElementById('videoPlayer');
+   const currentLevel = Number(level.value) - 1;
 
-    //if (currentLevel < levels.length && inputValue == levels[currentLevel].answer) {
-     //   alert("Answer correct");
-      //  score.value = Number(score.value) + 1;
-     //   level.value = Number(level.value) + 1; // Move to next level
+    if (currentLevel < levels.length && inputValue == levels[currentLevel].answer) {
+       alert("Answer correct");
+       score.value = Number(score.value) + 1;
+       level.value = Number(level.value) + 1; // Move to next level
 
-     //   if (levels[currentLevel].video) {
-       //     video.children[0].src = levels[currentLevel].video;
-       //     video.load();
-    //    }
-  //  } else {
- //       score.value = Number(score.value) - 1;
-   //     alert("Answer incorrect or wrong level!");
- //   }
+       if (levels[currentLevel].video) {
+           video.children[0].src = levels[currentLevel].video;
+           video.load();
+       }
+   } else {
+       score.value = Number(score.value) - 1;
+       alert("Answer incorrect or wrong level!");
+   }
 
-  //a  return false;
+  a  return false;
 }
